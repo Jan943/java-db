eb85b8e51b13ce1042c7afeaaf62eb295d8a8576
Jan943 <jansopata10@gmail.com>
2022-07-03 15:25:30 +0200

    Implementing CRUD actions:
    -creating Repository
    -adding Cars, deleting some of them and getting all thanks to methods of the Repository

---
+++ SpringData/src/main/java/pl/sopata/springdata/CarRepo.java
@@ -0,0 +1,10 @@
+package pl.sopata.springdata;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CarRepo extends CrudRepository<Car, Long> {
+}
---
+++ SpringData/src/main/java/pl/sopata/springdata/Start.java
@@ -0,0 +1,32 @@
+package pl.sopata.springdata;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+public class Start {
+
+    private CarRepo carRepo;
+
+    @Autowired
+    public Start(CarRepo carRepo) {
+        this.carRepo = carRepo;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void runExample() {
+        Car car = new Car("Honda", "Jazz", Color.BLUE);
+        carRepo.save(car);
+
+        Car car2 = new Car("Audi", "A1", Color.BLACK);
+        carRepo.save(car2);
+
+        Car car3 = new Car("Audi", "A2", Color.RED);
+        carRepo.save(car3);
+
+        carRepo.deleteById(1L);
+
+        Iterable<Car> all = carRepo.findAll();
+        all.forEach(System.out::println);
+    }
+}
